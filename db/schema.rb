2e8a860369796aadb1ae8cd1ad6836aac6143019
Jacob Rapp <jake.rapp322@gmail.com>
2016-08-01 08:46:32 -0700

    Create comment model and migration, basic form and display

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731041213) do
+ActiveRecord::Schema.define(version: 20160801152448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
